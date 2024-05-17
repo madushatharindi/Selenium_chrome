65b3a7036d9d85a918d61aaa821e9a39fe3fa6f5
madushatharindi <madushadesilvad6@gmail.com>
2024-05-17 13:32:32 +0530

    commit

---
+++ src/test/java/TextBoxExample.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TextBoxExample {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void openLinkTestPage(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/link.xhtml");
+
+    }
+
+    @Test
+    public void testBoxTests(){
+//        type your name
+        driver.findElement(By.id("j_idt88:name")).sendKeys("Madusha De Silva");
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
